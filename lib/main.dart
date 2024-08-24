5025c5cba145fe59c65936c7abc830bf319b7c21
penawjs <wsantana@sabesp.com.br>
2024-08-24 10:18:12 -0300

    Corrigido o crop: Antes de implementar thread para o crop

--- lib/main.dart
+++ lib/main.dart
@@ -24,21 +24,32 @@ class PolygonCropper extends StatefulWidget {
 
 class _PolygonCropperState extends State<PolygonCropper> {
   Uint8List? image;
+  var _showDialog = false;
 
   Future<void> _pickImage() async {
     final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
 
+    if (pickedFile != null) {
+      setImage(File(pickedFile.path).readAsBytesSync());
+    } else {
+      debugPrint('No image selected.');
+    }
+  }
+
+  void setImage(Uint8List image) {
     setState(() {
-      if (pickedFile != null) {
-        image = File(pickedFile.path).readAsBytesSync();
-      } else {
-        debugPrint('No image selected.');
-      }
+      this.image = image;
+    });
+  }
+
+  void showDialog(bool showDialog) {
+    setState(() {
+      _showDialog = showDialog;
     });
   }
 
   // Save the cropped image as a PNG file
-  void _saveImage(Uint8List bytes) async {
+  Future<void> _saveImage(Uint8List bytes) async {
     final directory = await getTemporaryDirectory();
     final imagePath = '${directory.path}/cropped_image.png';
     final imageFile = File(imagePath);
@@ -48,26 +59,86 @@ class _PolygonCropperState extends State<PolygonCropper> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Polygon Cropper'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.crop),
-            onPressed: _cropImage,
+    var size = MediaQuery.of(context).size;
+    var painter = PolyPainter(image: image);
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('Polygon Cropper'),
+            actions: [
+              IconButton(
+                  icon: const Icon(Icons.crop),
+                  onPressed: () async {
+                    // showDialog(true);
+                    setImage(await painter.cropImage());
+                    // showDialog(false);
+                  }
+              ),
+              IconButton(
+                  icon: const Icon(Icons.save),
+                  onPressed: () async {
+                    // showDialog(true);
+                    await _saveImage(await painter.cropImage());
+                    // showDialog(false);
+                  }
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Center(
-        child: image == null
-            ? const Text('No image selected.')
-            : ImagePlate(image: image!),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _pickImage,
-        tooltip: 'Pick Image',
-        child: const Icon(Icons.add_a_photo),
-      ),
+          body: Center(
+            child: image == null
+                ? const Text('No image selected.')
+                : painter.widget,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _pickImage,
+            tooltip: 'Pick Image',
+            child: const Icon(Icons.add_a_photo),
+          ),
+        ),
+        if(_showDialog)
+          Material(
+            color: Colors.black.withOpacity(0.5),
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                constraints: BoxConstraints(
+                  maxWidth: size.width * 0.8,
+                  maxHeight: size.height * 0.8,
+                  minWidth: size.width * 0.5,
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 10),
+                    const CircularProgressIndicator(),
+                    const SizedBox(height: 10),
+                    Text('Cropping image...',
+                      style: Theme.of(context).textTheme.bodyMedium
+                          ?.copyWith(color: Colors.black),
+                    ),
+                  ],
+                ),
+              )
+            ),
+          )
+      ]
     );
   }
+}
+
+Future<void> _dialogBuilder(BuildContext context) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return const AlertDialog(
+        title: Text('Cropping Image', style: TextStyle(fontSize: 16)),
+        content: Text('Wait...')
+      );
+    },
+  );
 }
--- lib/poly_paiter.dart
+++ lib/poly_paiter.dart
@@ -1,123 +1,157 @@
 
+import 'dart:async';
+import 'dart:isolate';
 import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
 
 class ImagePlate extends StatelessWidget {
   final Uint8List image;
-  final GlobalKey imageKey = GlobalKey();
+  final GlobalKey imageKey;
+  final PolyPainter painter;
 
-  ImagePlate({super.key, required this.image});
+  const ImagePlate({super.key, required this.image, required this.imageKey, required this.painter});
 
   @override
   Widget build(BuildContext context) {
-    var painter = PolyPainter(image: image, imageKey: imageKey);
-    return CustomPaint(
-      painter: painter,
-      child: GestureDetector(onTapUp: painter.addPoint,
-        child: Image.memory(image, key: imageKey)
-      ),
+    return Stack(
+      children: [
+        GestureDetector(onTapUp: painter.addPoint,
+            child: Image.memory(image, key: imageKey)),
+        CustomPaint(painter: painter),
+      ],
     );
   }
 }
 
+final _pointsCounter = ValueNotifier<int>(0);
+
 class PolyPainter extends CustomPainter {
-  Uint8List image;
-  GlobalKey imageKey;
-  final List<Offset> points = [];
-  var minX = 0;
-  var minY = 0;
-  var maxX = 0;
-  var maxY = 0;
-  var width = 0;
-  var height = 0;
-  var factorX = 0.0;
-  var factorY = 0.0;
+  Uint8List? image;
+  final GlobalKey _imageKey = GlobalKey();
+  final List<Offset> _points = [];
+  var _minX = 0;
+  var _minY = 0;
+  var _maxX = 0;
+  var _maxY = 0;
+  var _width = 0;
+  var _height = 0;
+  var _factorX = 0.0;
+  var _factorY = 0.0;
 
-  PolyPainter({required this.image, required this.imageKey});
+  ImagePlate? widget;
 
-  Uint8List cropImage() {
-    if (points.length < 3) {
-      return image;
+  PolyPainter({this.image}) : super(repaint: _pointsCounter) {
+    if(image != null) {
+      widget = ImagePlate(image: image!, imageKey: _imageKey, painter: this);
+    }
+  }
+
+  Future<Uint8List> cropImage() async {
+    debugPrint('enter cropImage: ${DateTime.now()}');
+    if (_points.length < 3 || image == null) {
+      return image ?? Uint8List(0);
     }
 
     // Determine image's scale factor
-    var mainImage = img.decodeImage(image);
-    var imageContext = imageKey.currentContext;
+    var mainImage = img.decodeImage(image!);
+    var imageContext = _imageKey.currentContext;
     if (imageContext != null) {
-      factorX = imageContext.size!.width / mainImage!.width;
-      factorY = imageContext.size!.height / mainImage.height;
+      _factorX = imageContext.size!.width / mainImage!.width;
+      _factorY = imageContext.size!.height / mainImage.height;
     }
 
     var mask = _makeMask(mainImage!);
-    return _crop(mainImage, mask);
+    var cimg = _crop(mainImage, mask);
+
+    // var cimg = await compute(_crop, mainImage!);
+    //
+    // final receivePort = ReceivePort();
+    // await Isolate.spawn(_crop, [mainImage, receivePort.sendPort]);
+    // final completer = Completer<Uint8List>();
+    // receivePort.listen((message) {
+    //   completer.complete(message);
+    //   receivePort.close();
+    // });
+    // var cimg = await completer.future;
+
+    debugPrint('exit cropImage: ${DateTime.now()}');
+    return cimg;
   }
 
+  // static void _crop(List<dynamic> args) async {
   img.Image _makeMask(img.Image image) {
+    debugPrint('enter _makeMask: ${DateTime.now()}');
+
     // Create a mask with the same size as the image
     final mask = img.Image(width: image.width, height: image.height);
     img.fill(mask, color: mask.getColor(0, 0, 0, 0));
 
+    final orderedPoints = _orderPoints(_points);
     // Draw the polygon on the mask applying the scale factor
-    final vertices = points.map((point) => img.Point((point.dx/factorX).toInt(), (point.dy/factorY).toInt())).toList();
+    final vertices = orderedPoints.map((point) => img.Point((point.dx/_factorX).toInt(), (point.dy/_factorY).toInt())).toList();
     img.fillPolygon(mask, vertices: vertices, color: mask.getColor(255, 255, 255, 255));
 
     // The size of the cropped image is defined by the difference between the largest and smallest x and y values.
-    minX = vertices.map((point) => point.xi).reduce(min);
-    minY = vertices.map((point) => point.yi).reduce(min);
-    maxX = vertices.map((point) => point.xi).reduce(max);
-    maxY = vertices.map((point) => point.yi).reduce(max);
-    width = maxX - minX;
-    height = maxY - minY;
+    _minX = vertices.map((point) => point.xi).reduce(min);
+    _minY = vertices.map((point) => point.yi).reduce(min);
+    _maxX = vertices.map((point) => point.xi).reduce(max);
+    _maxY = vertices.map((point) => point.yi).reduce(max);
+    _width = _maxX - _minX;
+    _height = _maxY - _minY;
 
+    debugPrint('exit _makeMask: ${DateTime.now()}');
     return mask;
   }
 
+
+  // static void _crop(List<dynamic> args) async {
+  //   final img.Image image = args[0];
+    // final img.Image mask = args[1];
+    // final SendPort sendPort = args[2];
+
   Uint8List _crop(img.Image image, img.Image mask) {
+    debugPrint('enter _crop: ${DateTime.now()}');
+
     // Apply the mask to the image
-    final cropped = img.Image(width: width, height: height, numChannels: 4);
+    final cropped = img.Image(width: _width, height: _height, numChannels: 4);
+    var white = mask.getColor(255, 255, 255, 255);
     img.fill(cropped, color: mask.getColor(0, 0, 0, 0));
     for (int y = 0; y < image.height; y++) {
       for (int x = 0; x < image.width; x++) {
         final maskPixel = mask.getPixelLinear(x, y);
-        if (maskPixel == mask.getColor(255, 255, 255, 255)) {
-          cropped.setPixel(x-minX, y-minY, image.getPixelLinear(x, y));
+        if (maskPixel == white) {
+          cropped.setPixel(x-_minX, y-_minY, image.getPixelLinear(x, y));
         }
       }
     }
 
-    return img.encodePng(cropped);
+    debugPrint('encoding _crop: ${DateTime.now()}');
+    var cimg = img.encodePng(cropped);
+    debugPrint('exit _crop: ${DateTime.now()}');
+
+    // sendPort.send(cimg);
+
+    return cimg;
   }
 
-  List<Offset> orderPoints(List<Offset> points) {
-    // find the point with the lowest x and y
-    final Offset initialPoint = points.reduce((a, b) => a.dx < b.dx ? a : b);
+  List<Offset> _orderPoints(List<Offset> points) {
+    final center = Offset(
+        points.map((point) => point.dx)
+                     .reduce((a, b) => a + b) / points.length,
+        points.map((point) => point.dy)
+                     .reduce((a, b) => a + b) / points.length);
 
-    // sort the points by y value
-    points.sort((a, b) => a.dx == b.dx ? (a.dy - b.dy).toInt() : (a.dx - b.dx).toInt());
+    final List<Offset> leftPoints = List.from(points.where((point) => point.dx < center.dx));
+    leftPoints.sort((a, b) => a.dy > b.dy ? 1 : a.dy < b.dy ? -1 : 0);
 
-    // find the index of the initial point
-    final int initialIndex = points.indexOf(initialPoint);
+    final List<Offset> rightPoints = List.from(points.where((point) => point.dx > center.dx));
+    rightPoints.sort((a, b) => a.dy < b.dy ? 1 : a.dy > b.dy ? -1 : 0);
 
-    // create a list to store the ordered points
-    final List<Offset> orderedPoints = [];
-
-    // add the initial point to the ordered points
-    orderedPoints.add(initialPoint);
-
-    // add the points with y value greater than the initial point
-    for (int i = initialIndex + 1; i < points.length; i++) {
-      orderedPoints.add(points[i]);
-    }
-
-    // add the points with y value less than or equal to the initial point
-    for (int i = 0; i < initialIndex; i++) {
-      orderedPoints.add(points[i]);
-    }
-
-    return orderedPoints;
+    return leftPoints + rightPoints;
   }
 
 // https://stackoverflow.com/questions/55083414/drawing-on-canvas-combined-with-gesturedetector
@@ -125,27 +159,25 @@ class PolyPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    if(points.isEmpty) return;
+    if(_points.isEmpty) return;
 
-    // if points are more than 2, clear previous painted lines
-    if (points.length >= 2) {
-      _paintDots(Colors.transparent, Colors.transparent, canvas, points);
-      _traceLines(Colors.transparent, canvas, points);
+    if(_points.length == 1) {
+      _paintDots(Colors.white10, Colors.red, canvas, _points);
+      return;
     }
 
-    final orderedPoints = orderPoints(points);
+    final orderedPoints = _orderPoints(_points);
 
     // Paint the dots
     _paintDots(Colors.white10, Colors.red, canvas, orderedPoints);
-
-    if (orderedPoints.length < 2) return;
 
     // Draw Lines
     _traceLines(Colors.red, canvas, orderedPoints);
   }
 
   void addPoint(TapUpDetails details) {
-    points.add(details.localPosition);
+    _points.add(details.localPosition);
+    _pointsCounter.value = _points.length;
   }
 
   void _paintDots(Color outerColor, Color innerColor, Canvas canvas, List<Offset> points) {
@@ -173,7 +205,7 @@ class PolyPainter extends CustomPainter {
     for (int i = 0; i < points.length - 1; i++) {
       canvas.drawLine(points[i], points[i + 1], paint);
     }
-    path.close();
+    canvas.drawLine(points[points.length-1], points[0], paint);
 
     canvas.drawPath(path, paint);
   }
